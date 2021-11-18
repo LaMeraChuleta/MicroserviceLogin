dc84cbc5bfb422bff5a3c532fa7207b383f3e7a8
LaMeraChuleta <geffreyytorres25@gmail.com>
2021-11-18 11:19:56 -0600

    Comminit inicial

---
+++ MicroserviceLogin/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroserviceLogin.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+
+        private readonly ILogger<LoginController> _logger;
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public ActionResult Token()
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("MY_SECRET_KEY_LOGIN_hjkHKy76VF6r8FEDhgfhG");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, "LaMeraChuleta")
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new { token = tokenString });
+        }
+        
+    }
+}
